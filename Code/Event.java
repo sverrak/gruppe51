245ced56dd583d2ab1c03f9d482cdfd880bf8e03
SVERRAK <SVERRE_ROSICKY@HOTMAIL.COM>
2015-02-23 14:14:24 +0100

    fikset klasser

--- Code/Event.java
+++ Code/Event.java
@@ -1,11 +1,31 @@
 package Code;
 
+import java.util.Collection;
 import java.util.Date;
 
 public class Event {
 	private String title;
 	private Date startTime;
 	private Date endTime;
+	private String description;
+	private String image;
 	
+	public void fireChange(){
+		
+	}
+	
+	public Collection<Room> findLocation(){
+		
+		
+		return null;
+	}
+	
+	public boolean addEmployee(){
+		return true;
+	}
+	
+	public boolean removeEmployee(){
+		return true;
+	}
 	
 }
